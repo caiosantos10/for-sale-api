e253fd58ac7a8ab0fd8ddde8c4c977ec04f121ac
Caio Santos <caiosant10@gmail.com>
2025-01-31 21:15:08 -0300

    Alterei migration e acrescentei coluna price na tabela PRoduto

--- src/migrations/1738201565507-CreateProducts.ts
+++ src/migrations/1738201565507-CreateProducts.ts
@@ -8,6 +8,7 @@ export class CreateProducts1738201565507 implements MigrationInterface {
                 id UUID PRIMARY KEY NOT NULL,
                 name VARCHAR(100) NOT NULL,
                 description VARCHAR(200),
+                price DECIMAL(10, 2) NOT NULL,
                 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                 updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
             );
