56d5e97648bca85327f48a3f456c67cdb44fdebd
Caio Santos <caiosant10@gmail.com>
2025-04-23 22:46:09 -0300

    [feature-45] migration e entity CartProducts

--- src/modules/cart/entities/Cart.ts
+++ src/modules/cart/entities/Cart.ts
@@ -1,10 +1,21 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import Users from "@modules/users/entities/Users";
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn
+} from "typeorm";
 
 @Entity('Carts')
 export default class Cart {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
+    @ManyToOne(() => Users)
+    @JoinColumn({ name: 'user_id' })
     @Column()
     user_id: string;
 
