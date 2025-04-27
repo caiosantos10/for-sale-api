d6faf74add32045dc23206195cf624b9f247049d
Caio Santos <caiosant10@gmail.com>
2025-04-27 20:12:57 -0300

    [feature-61] products nas responses de cart

--- src/modules/cart/services/FindOneCartService.ts
+++ src/modules/cart/services/FindOneCartService.ts
@@ -1,17 +1,28 @@
 import AppError from "src/shared/errors/AppError";
 import CartRepository from "../repositories/CartRepository";
-import Cart from "../entities/Cart";
+import { CartResponseDTO } from "../utils/cart-response.dto";
 
 interface IRequest {
     id: string;
 }
 
 export default class FindOneCartService {
-    public async execute({ id }: IRequest): Promise<Cart> {
-        const cart = await CartRepository.findOne({ where: { id } });
+    public async execute({ id }: IRequest): Promise<CartResponseDTO> {
+        const cart = await CartRepository.findOne({
+            where: { id },
+            relations: ['cartProducts', 'cartProducts.product']
+        });
 
         if (!cart) throw new AppError('Cart not found.');
 
-        return cart;
+         const cartResponse: CartResponseDTO = {
+            id: cart.id,
+            user_id: cart.user_id,
+            created_at: cart.created_at,
+            updated_at: cart.updated_at,
+            products: cart?.cartProducts.map(cartProduct => (cartProduct.product)) ?? [],
+        };
+
+        return cartResponse;
     }
 }
