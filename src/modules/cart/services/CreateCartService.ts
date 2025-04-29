5cc0defc930c1dc25c01472d1445c43f6bc3ed0f
Caio Santos <caiosant10@gmail.com>
2025-04-29 17:01:25 -0300

    [feature-62] Um carrinho por vez, por usuário

--- src/modules/cart/services/CreateCartService.ts
+++ src/modules/cart/services/CreateCartService.ts
@@ -4,6 +4,7 @@ import CartProductsRepository from "../repositories/CartProductsRepository";
 import { CartResponseDTO } from "../utils/cart-response.dto";
 import ProductRepository from "@modules/products/repositories/ProductRepository";
 import { In } from "typeorm";
+import UpdateCartService from "./UpdateCartService";
 
 interface IRequest {
     user_id: string;
@@ -15,37 +16,49 @@ export default class CreateCartService {
         if (product_ids.length <= 0) {
             throw new AppError('You must pass one or more products to create a Cart.', 400);
         }
-        
-        // Criar um Cart
-        const cart = CartRepository.create({ user_id });
-        await CartRepository.save(cart);
 
-        // Validar se todos os produtos existem
-        const products = await ProductRepository.findBy({ id: In(product_ids) });
-        if (products.length !== product_ids.length) {
-            throw new Error('One or more products not found');
+        let cartResponse: CartResponseDTO;
+
+        // If there is a cart by user, update it with product ids
+        const cartAlreadyExists = await CartRepository.findOne({ where: { user_id } });
+        if (cartAlreadyExists) {
+            const updateCartService = new UpdateCartService()
+            cartResponse = await updateCartService.execute({
+                id: cartAlreadyExists.id,
+                product_ids,
+            });
+        } else {
+            // If there is no cart by user, create one
+            const cart = CartRepository.create({ user_id });
+            await CartRepository.save(cart);
+
+            // Valid if all products exist
+            const products = await ProductRepository.findBy({ id: In(product_ids) });
+            if (products.length !== product_ids.length) {
+                throw new Error('One or more products not found');
+            }
+
+            // Create a CartProducts for each Product 
+            const cartProducts = products.map(product => CartProductsRepository.create({
+                cart_id: cart.id,
+                product_id: product.id,
+            }));
+            await CartProductsRepository.save(cartProducts);
+
+            // Get the whole Cart with Products attached
+            const cartComplete = await CartRepository.findOne({
+                where: { id: cart.id },
+                relations: ['cartProducts', 'cartProducts.product'],
+            });
+
+            cartResponse = {
+                id: cart.id,
+                user_id: cart.user_id,
+                created_at: cart.created_at,
+                updated_at: cart.updated_at,
+                products: cartComplete?.cartProducts.map(cartProduct => (cartProduct.product)) ?? [],
+            };
         }
-
-        // Criar um registro de CartProducts para cada produto adicionado no carrinho
-        const cartProducts = products.map(product => CartProductsRepository.create({
-            cart_id: cart.id,
-            product_id: product.id,
-        }));
-        await CartProductsRepository.save(cartProducts);
-
-        // Buscar novamente o cart completo, já com products
-        const cartComplete = await CartRepository.findOne({
-            where: { id: cart.id },
-            relations: ['cartProducts', 'cartProducts.product'],
-        });
-
-        const cartResponse: CartResponseDTO = {
-            id: cart.id,
-            user_id: cart.user_id,
-            created_at: cart.created_at,
-            updated_at: cart.updated_at,
-            products: cartComplete?.cartProducts.map(cartProduct => (cartProduct.product)) ?? [],
-        };
 
         return cartResponse;
 
