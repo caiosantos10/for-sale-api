7ec6e09c695b3e2560d9c98368889db652a6b906
Caio Santos <caiosant10@gmail.com>
2025-06-06 09:35:05 -0300

    [feature-95] criando listagem de purchases

--- src/modules/purchase/services/ListPurchasesService.ts
+++ src/modules/purchase/services/ListPurchasesService.ts
@@ -1,10 +1,36 @@
-import Purchase from "../entities/Purchase";
 import PurchaseRepository from "../repositories/PurchaseRepository";
+import { PurchaseResponseDTO } from "../utils/purchase.dto";
 
 export default class ListPurchasesService {
-    public async execute(): Promise<Purchase[]> {
-        const Purchases = await PurchaseRepository.find();
+    public async execute(): Promise<PurchaseResponseDTO[]> {
+        const purchases = await PurchaseRepository
+            .createQueryBuilder('purchase')
+            .leftJoinAndSelect('purchase.purchaseProducts', 'purchaseProduct')
+            .leftJoinAndSelect('purchaseProduct.product', 'product')
+            .leftJoinAndSelect('purchase.paymentMethod', 'paymentMethod')
+            .getMany();
+
         
-        return Purchases;
+        return purchases.map(item => ({
+            id: item.id,
+            user_id: item.user_id,
+            products: item.purchaseProducts?.map(pp => ({
+                id: pp.product.id,
+                name: pp.product.name,
+                description: pp.product.description,
+                price: pp.product.price,
+                image: pp.product.image,
+                quantity: pp.quantity,
+                observations: pp.observations,
+            })) ?? [],
+            status: item.status,
+            delivery_address: item.delivery_address,
+            payment_method: {
+                id: item.paymentMethod.id,
+                method: item.paymentMethod.method,
+                installments: item.paymentMethod.installments,
+                cardBrand: item.paymentMethod.card_brand,
+            }
+        }));
     }
 }
