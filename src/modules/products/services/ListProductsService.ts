4c4666c428a3b3366e7f41df4be6d7f2ff75cc7a
Caio Santos <caiosant10@gmail.com>
2025-06-16 23:12:49 -0300

    [feature-102] paginação hard coded na listagem de produtos

--- src/modules/products/services/ListProductsService.ts
+++ src/modules/products/services/ListProductsService.ts
@@ -1,10 +1,25 @@
+import { PageList } from "@shared/interfaces/page-list.interface";
 import Product from "../entities/Product";
 import ProductRepository from "../repositories/ProductRepository";
 
+const page = 1;
+const perPage = 10;
+
 export default class ListProductsService {
-    public async execute(): Promise<Product[]> {
-        const products = await ProductRepository.find();
+    // Ajustar tipagem do retorno para interface que tenha objeto e elementos da paginação
+    public async execute(): Promise<PageList<Product>> {
+        const [products, total] = await ProductRepository.findAndCount({
+            skip: (page - 1) * perPage,
+            take: perPage,
+            order: { name: 'ASC' }, 
+            // where: { active: true },
+        });
         
-        return products;
+        return {
+            data: products,
+            total,
+            page,
+            lastPage: Math.ceil(total / perPage),
+        };
     }
 }
