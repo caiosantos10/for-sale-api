ae89cc0354c898c01cf8dce2e68cf55aa51b892c
Caio Santos <caiosant10@gmail.com>
2025-06-20 16:08:35 -0300

    [feature-103] paginação no list users

--- src/modules/users/services/ListUsersService.ts
+++ src/modules/users/services/ListUsersService.ts
@@ -1,10 +1,23 @@
+import { PageList } from "@shared/interfaces/page-list.interface";
 import User from "../entities/Users";
 import UsersRepository from "../repositories/UsersRepository";
 
+const page = 1;
+const perPage = 10;
+
 export default class ListUsersService {
-    public async execute(): Promise<User[]> {
-        const users = await UsersRepository.find();
+    public async execute(): Promise<PageList<User>> {
+        const [users, total] = await UsersRepository.findAndCount({
+            skip: (page - 1) * perPage,
+            take: perPage,
+            order: { name: 'ASC' },
+        });;
         
-        return users;
+        return {
+            data: users,
+            total,
+            page,
+            lastPage: Math.ceil(total / perPage),
+        };
     }
 }
